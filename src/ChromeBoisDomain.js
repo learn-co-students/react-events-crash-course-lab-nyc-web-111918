0cf58c14f4f18c2cae3621e8b0c9b9d86ada4d32
aschemily <aschemily@gmail.com>
2019-01-28 17:58:30 -0500

    Done.

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -3,31 +3,50 @@ import { drawChromeBoiAtCoords, toggleCycling, resize } from './canvasHelpers.js
 
 
 export default class ChromeBoisDomain extends Component {
-  
+
   handleMouseMove = (event) => {
+    console.log(event.clientX, event.clientY)
     /* TODO: This method should capture the `x` and `y` coordinates of the mouse
      * from the event and use them to invoke the `drawChromeBoiAtCoords`
      * function that has been provided and is already imported
      * (`drawChromeBoiAtCoords` expects two arguments, an x and a y coordinate)
      */
+     //console.log(drawChromeBoiAtCoords(event.clientX, event.clientY))
+     drawChromeBoiAtCoords(event.clientX, event.clientY)
   }
-  
+
+  onMouseClick = (event) => {
+    //console.log(event)
+    toggleCycling();
+  }
+
   /* TODO: Create an event handler which, when fired, invokes the provided
    * `toggleCycling` function with no arguments. Don't forget the click event
    * listener that should fire it!
    */
-   
+
+   onKeyPress = (event) =>{
+     //console.log(event.key)
+     if (event.key === "a"){
+       resize("+")
+     } else if (event.key === "s"){
+       resize("-")
+     }
+
+   }
   /* TODO: Add an event listener to the `<canvas>` element to capture when a key
   /* is pressed. When a key is pressed, an event handler should invoke the the
   /* provided `resize` function with a single argument of either '+' or '-'
   /* if the key pressed was 'a', then it should call `resize` with '+'
-  /* if the key pressed was 's', then it should call `resize` with '-' 
+  /* if the key pressed was 's', then it should call `resize` with '-'
    */
-  
+
   render() {
     return (
-      <canvas 
+      <canvas
         onMouseMove={this.handleMouseMove}
+        onClick={this.onMouseClick}
+        onKeyPress={this.onKeyPress}
         width='900'
         height='600'
         tabIndex="0">
